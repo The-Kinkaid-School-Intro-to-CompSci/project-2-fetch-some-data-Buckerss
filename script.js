2123c6b3e99e8dc8f280384d2eff2374a66d7cda
Buckerss <abby.woo@kinkaid.org>
2024-05-07 14:46:05 -0500

    Create president json and begin making containers

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+async function fetchJSONData(fileName){
+
+    const response = await fetch(`data/presidents.json`);
+    console.log("Response: ", response);
+    console.log("Response status: ", response.body);
+
+    // if (response.status !== 200){
+    //     alert(`HTTP error! status: ${response.status}`);
+    //     throw new Error(`HTTP error! status: ${response.status}`);
+    // }
+    const data = await response.json();
+    console.log("Data:", data);
+    return data;
+
+}
+
 function runProgram(){
 
     console.log('runProgram');
